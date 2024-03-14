03819865a2aae0c5808d2f1a09aa21ef5760c663
SG-stellar <150489384+SG-stellar@users.noreply.github.com>
2024-03-13 20:04:14 -0700

    Add files via upload

---
+++ myApp_test.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import os
+import pandas as pd
+
+
+st.title("My Personal Doc Reader")
+st.write("Upload the doc below:")
+
+
+uploaded_file = st.file_uploader("Choose a document pdf file")
+if uploaded_file is not None:
+
+    pass
